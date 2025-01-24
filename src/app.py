6aa111c320d7dbe853015b40c89eebdc72a09784
Nicolas L <nicolas.loisy@yahoo.fr>
2025-01-24 19:33:06 +0100

    Update Flask app to run on port 8000 instead of 5000

--- src/app.py
+++ src/app.py
@@ -24,4 +24,4 @@ def decode():
     return jsonify({'decoded_message': decoded_message})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
